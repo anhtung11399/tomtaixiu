39d05942b94c1317c1f5a4d944acd22c92ba8ef6
anhtung11399 <74240280+anhtung11399@users.noreply.github.com>
2023-08-25 14:54:49 +0700

    Delete taixiu directory

--- taixiu/script.js
+++
@@ -1,9 +0,0 @@
-// script.js
-const diceResult = document.getElementById('dice-result');
-const rollButton = document.getElementById('roll-button');
-const diceImage = document.getElementById('dice-image');
-
-rollButton.addEventListener('click', () => {
-  const randomValue = Math.floor(Math.random() * 6) + 1;
-  diceImage.src = `dice${randomValue}.png`;
-});
